7810da9211434f375e0d04c5844c449732b67be9
HardPlant <abc7988se@naver.com>
2017-09-28 22:13:58 +0900

    declared encode

--- NLTK/pyNLTK.py
+++ NLTK/pyNLTK.py
@@ -1,7 +1,9 @@
+#-*- coding: utf-8 -*-
 from __future__ import division
 import nltk
 from nltk.corpus import PlaintextCorpusReader # Corpus => 말뭉치
 rootOfCorpus = 'e://simpson//'
 
 newCorpus = PlaintextCorpusReader(rootOfCorpus, '.*')
+print type(newCorpus)
 
