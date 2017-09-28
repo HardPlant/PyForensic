03824f1863cc2cfb388143b58eee60ddd2a6cf46
HardPlant <abc7988se@naver.com>
2017-09-28 22:12:22 +0900

    assign corpus root, and collect it with PlainTextCorpusReader

---
+++ NLTK/pyNLTK.py
@@ -0,0 +1,7 @@
+from __future__ import division
+import nltk
+from nltk.corpus import PlaintextCorpusReader # Corpus => 말뭉치
+rootOfCorpus = 'e://simpson//'
+
+newCorpus = PlaintextCorpusReader(rootOfCorpus, '.*')
+
