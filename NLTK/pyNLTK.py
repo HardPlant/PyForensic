da5df700566c163e344fbfe8e0fd2b42b630b02b
HardPlant <abc7988se@naver.com>
2017-09-28 22:24:58 +0900

    print collocations (연어 목록 - 의미를 만드는 짝?)

--- NLTK/pyNLTK.py
+++ NLTK/pyNLTK.py
@@ -3,6 +3,7 @@ from __future__ import division
 import nltk
 from nltk.corpus import PlaintextCorpusReader # Corpus => 말뭉치
 nltk.download('punkt')
+nltk.download('stopwords')
 
 rootOfCorpus = 'e://simpson//'
 
@@ -12,3 +13,5 @@ tokens = nltk.word_tokenize(rawText)
 textSimpson = nltk.Text(tokens)
 vocabularyUsed = set(textSimpson)
 myWord = "KILL"
+
+print textSimpson.collocations()
