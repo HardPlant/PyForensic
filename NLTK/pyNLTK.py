b5ff2757bf9f58314ec67fd7891db8f0eb5bc517
HardPlant <abc7988se@naver.com>
2017-09-28 22:18:15 +0900

    tokenize data,

--- NLTK/pyNLTK.py
+++ NLTK/pyNLTK.py
@@ -2,6 +2,8 @@
 from __future__ import division
 import nltk
 from nltk.corpus import PlaintextCorpusReader # Corpus => 말뭉치
+nltk.download('punkt')
+
 rootOfCorpus = 'e://simpson//'
 
 newCorpus = PlaintextCorpusReader(rootOfCorpus, '.*')
@@ -13,5 +15,6 @@ print newCorpus.abspaths()
 rawText = newCorpus.raw()
 print len(rawText)
 
-
-
+tokens = nltk.word_tokenize(rawText)
+print len(tokens)
+print tokens[0:100]
